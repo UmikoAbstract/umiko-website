dfe0fbeeec380b30c275e6c64e23bd5cb4684ed8
UmikoAbsctract <umikoabstract@gmail.com>
2025-01-28 23:19:17 +0200

    s

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
 import "../global.css";
-import { Inter } from "next/font/google";
-import LocalFont from "next/font/local";
 import { Metadata } from "next";
 export const metadata: Metadata = {
   title: {
